fb08b97c5bd721c3cc05d111c3e7917803b66ffd
angpace <108838753+angpace@users.noreply.github.com>
2022-11-02 17:31:56 -0400

    commit tree b8215b43df3c7f5828a97cd8c906bab69032276d

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,58 @@
+class TicTacToe
+attr_reader :board 
+attr_accessor :count
+
+WIN_COMBINATIONS = [
+    [0,1,2], # Top row
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [6,4,2]
+]
+
+
+
+def initialize 
+    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    @@count = 8
+end
+
+def display_board 
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(stg)
+    stg.to_i - 1
+
+end
+
+def move (input, player = "X")
+ @board[input] = player
+ 
+end
+
+def position_taken? (input)
+    @board[input] == "X" || @board[input] == "O"
+    
+end
+
+def valid_move?(index)
+ if index.between?(0, 8) && !position_taken?(index)
+     true
+ else
+    false
+ end
+end
+
+def turn_count 
+   @board.filter { |index| index != " " }.length
+end
+
+end
